1186fc450e4ccc07d5fa0923da39a6ac917b765b
eightseconds <paulseokcho@gmail.com>
2019-09-30 16:35:24 -0700

    update solution

--- insertionSort.js
+++ insertionSort.js
@@ -1,11 +1,10 @@
 function insertionSort(array) {
 	for (let i = 0; i < array.length; i++) {
 		let j = i
-		while (j > 0 && array[j] < array[j - 1]) {
-			
+		while (j > 0 && array[j] < array[j - 1]) {	
 			let swap = array[j]
 			array[j] = array[j - 1]
-			array[j - 1]= swap
+			array[j - 1] = swap
 			j--
 		}
 	}
